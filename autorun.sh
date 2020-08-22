ba321a3b6f6117f419ff7698ea7e3b15cceeebd9
Theis <theisc@pm.me>
2020-08-22 06:49:11 +0200

    added my Awesome config

---
+++ autorun.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#run flameshot
+run redshift
+#run guake
+#run vm-nat
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Awesome
+rm -rf /home/$USER/.config/awesome
+ln -s /home/$USER/.config/awesomewm /home/$USER/.config/awesome
